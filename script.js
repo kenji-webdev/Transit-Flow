de11e278c5d5bffe461c1d8597562751fa923704
Kenji.N <1314webdev@gmail.com>
2023-03-29 16:45:23 -0300

    Add NavMenu

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".nav__hamburger");
+const menu = document.querySelector(".nav__content");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("open");
+});
+const navLinks = document
+  .querySelectorAll(".nav__pages__link")
+  .forEach((link) => {
+    if (link.href === window.location.href) {
+      console.log(link.href);
+      console.log(window.location.href);
+      link.classList.add("active");
+    }
+  });
